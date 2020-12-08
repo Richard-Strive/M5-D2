87d2d0f0707d4141bd2d2f0f63abaddc59da4b42
Richard-Strive <72205808+Richard-Strive@users.noreply.github.com>
2020-12-08 21:41:47 +0100

    ok

--- src/students/index.js
+++ src/students/index.js
@@ -1,15 +1,28 @@
 const { Router } = require("express");
 const express = require("express");
+/* Third party elementi non integrati che devono essere importati */
 
 const router = express.Router();
 /* Posso creare piu endpoint: router.get(""), router.post("") e cosi via. In sintessi e' un percorso che mi permette di fare 
 determinate azioni al contento delle nostre "array" */
 
+const path = require("path");
+/* core module integrati  */
+
 const fs = require("fs");
-/* core module NON SO CHE SIGNIFICA E A COSA SERVE*/
+/* core module integrato su express*/
 
 router.get("/", (req, res) => {
-  console.log(req);
+  const studentsFilePath = "";
+  console.log(
+    __dirname
+  ); /* directory dinamica che serve a creare il path in funzione dal sistema da cui ci stiamo accededno */
+  console.log(
+    path.join(__dirname, "students.json")
+  ); /* l'unico modo per concatenare le path "path.join(__dirname, "students.json")"
+  /* a) stiamo creando un percorso che ci permette di ritirare i dati dal students.json visto che non abbiamo ancora un database*/
+
+  // console.log(req);
   res.send("I AM THE RESPONSE: LIST OF STUDENTS");
 });
 /* dal server ricevo localhost:3001/users/ e qui sopra sto creando una funzione che mi permette di ricevere risposta al percorso a aggiuntivo di "/"*/
@@ -23,6 +36,7 @@ router.post("/", (req, res) => {
   console.log(req);
   res.send("I AM THE RESPONSE: STUDENT POST CREATED");
 });
+
 /*PUT*/
 router.put("/", (req, res) => {
   console.log(req);
